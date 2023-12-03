6f7d166641cfdf2c36f37cac43a0b75a99e6a257
Aarush <DksAarush@gmail.com>
2023-12-03 00:12:46 -0500

    Added Comments

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+//imports and required modules
 const express = require('express');
 const app = express();
 
@@ -10,23 +11,34 @@ db.serialize(() => {
 	db.run('CREATE TABLE IF NOT EXISTS ids (id INTEGER PRIMARY KEY)');
 });
 
+//Keywords: 
+//post => send data to the server
+//get => get data from the server
+//req = request
+//res = response
+
+//gets the request from the client and sends it to the database
 app.post('/addID', (req, res) => {
 	const id = req.body.id;
 	console.log("sentData " + id);
 
+	//adds the id to the database
 	db.run('INSERT INTO ids (id) VALUES (?)', [id], (err) => {
 		if (err) {
 			console.error(err);
 			res.status(500).send('Error inserting ID into database');
 		} else {
+			//sends back a response to the client
 			res.send('ID inserted successfully');
 			console.log("ID inserted successfully");
 		}
 	});
 });
 
+//gets the request from the client and sends it to the database
 app.delete('/deleteID', (req, res) => {
 	const id = req.body.id;
+	//delete the requested id from the database
 	db.run('DELETE FROM ids WHERE id = ?', [id], (err) => {
 		if (err) {
 			console.error(err);
@@ -38,6 +50,7 @@ app.delete('/deleteID', (req, res) => {
 	});
 });
 
+//checks if the id exists in the database
 app.post('/checkID', (req, res) => {
 	const id = req.body.id;
 	console.log("checkData " + id);
@@ -46,6 +59,7 @@ app.post('/checkID', (req, res) => {
 			console.error(err);
 			res.status(500).send('Error retrieving IDs from database');
 		} else {
+			//if the id exists, send true otherwise send false
 			console.log(id);
 			if (rows.length > 0) {
 				console.log("ID exists");
@@ -58,6 +72,7 @@ app.post('/checkID', (req, res) => {
 	});
 });
 
+//gets all the ids from the database
 app.get('/getIDs', (req, res) => {
 	db.all('SELECT * FROM ids', (err, rows) => {
 		if (err) {
@@ -70,7 +85,9 @@ app.get('/getIDs', (req, res) => {
 	});
 });
 
-app.delete('/deleteAllIds', (req, res) => {
+//deletes all the ids from the database
+app.delete('/deleteAll', (req, res) => {
+	console.log("Deleting all IDs");
 	db.run('DELETE FROM ids', (err) => {
 		if (err) {
 			console.error(err);
@@ -82,4 +99,5 @@ app.delete('/deleteAllIds', (req, res) => {
 	});
 });
 
-app.listen(5000, () => console.log('Example app is listening on port 3000.'));
+//starts the server
+app.listen(5000, () => console.log('Example app is listening on port 500.'));
